c4db2b5130723c19b3d76da6aee2a4a10eb09c97
Himisha-G <himisha2009@gmail.com>
2026-03-08 06:46:43 +0530

    check login button

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -13,11 +13,14 @@ export default function LoginPage() {
       <h1 className="text-xl">Login to MediaMitra</h1>
 
       <button
-        onClick={handleLogin}
-        className="bg-green-500 px-6 py-3 rounded text-black"
-      >
-        Login / Signup
-      </button>
+  onClick={() => {
+    window.location.href =
+      "https://YOUR_DOMAIN.auth.ap-southeast-2.amazoncognito.com/login?client_id=YOUR_CLIENT_ID&response_type=code&scope=email+openid+profile&redirect_uri=https://main.d3qsdg65dn72cr.amplifyapp.com/login";
+  }}
+  className="bg-green-500 px-6 py-3 rounded text-black"
+>
+  Login / Signup
+</button>
     </div>
   )
 }
