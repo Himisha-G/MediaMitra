bc508077e7fe543228e82d768f8cc611ace27dfd
Himisha-G <himisha2009@gmail.com>
2026-03-08 05:46:30 +0530

    next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  images: {
-    unoptimized: true,
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
